eacf62d1902953919a78b5db2052838b3aab8be8
sr23 <qianfu@qianqiasmacbook.home>
2014-05-26 12:04:32 +0100

    init

---
+++ beeproto/beeproto/BeeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BeeAppDelegate.h
+//  beeproto
+//
+//  Created by Qianqian Fu on 5/26/14.
+//  Copyright (c) 2014 SR23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BeeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ beeproto/beeproto/BeeMyScene.m
@@ -0,0 +1,29 @@
+//
+//  BeeMyScene.m
+//  beeproto
+//
+//  Created by Qianqian Fu on 5/26/14.
+//  Copyright (c) 2014 SR23. All rights reserved.
+//
+
+#import "BeeMyScene.h"
+
+@implementation BeeMyScene
+
+-(id)initWithSize:(CGSize)size {    
+    if (self = [super initWithSize:size]) {
+        /* Setup your scene here */
+
+    }
+    return self;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    /* Called when a touch begins */
+}
+
+-(void)update:(CFTimeInterval)currentTime {
+    /* Called before each frame is rendered */
+}
+
+@end
