c9d3a7daa7532a05114b902186165776331fa3ef
PaulMac <paul@pauls-macbook.home>
2014-05-30 22:25:34 +0100

    more game ideas

--- beeproto/beeproto/BeeMyScene.h
+++ beeproto/beeproto/BeeMyScene.h
@@ -6,6 +6,20 @@
 //  Copyright (c) 2014 SR23. All rights reserved.
 //
 
+/*
+ * Game ideas:
+ *  * Fly as a swarm rather than single bee
+ *    * Invidual bees can be lost and form "health"
+ *    * Must have at least one bee to end the level
+ *  * Collect pollen from flowers (cute) on the ground or from trees high up
+ *  * Pollen forms the game's "currency"
+ *    * Can be spent on buying more bees or making existing bees stronger
+ *    * Can be spent on hive upgrades which will lead to more benefits
+ *  * Enemies: wasps, pollution clouds, something else
+ *  * Contact with an enemy leads to loss of one of the bees from the swarm
+ *  * Levels will have fixed length so that it is possible to reach the end and "return to base"
+ */
+
 #import <SpriteKit/SpriteKit.h>
 
 @interface BeeMyScene : SKScene
